9a5dcabc7c337f25ee5f33f33224af998aeeef5e
strashacc <sayantrashacc@gmail.com>
2025-05-24 20:39:39 +0500

    Added postgresql host

--- Task/db/db.go
+++ Task/db/db.go
@@ -11,7 +11,7 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := "host=localhost user=postgres password=yourpass dbname=yourdb port=5432 sslmode=disable"
+	dsn := "host=task_db user=postgres password=qwerty dbname=tasks port=5432 sslmode=disable"
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
